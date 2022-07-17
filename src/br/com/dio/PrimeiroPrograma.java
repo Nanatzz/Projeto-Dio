f36588fb4b91c4e555d5fd584e7f76033838d0e3
Nanatzz <natygrott@hotmail.com>
2022-07-17 19:47:54 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cat gato = new Cat();
+
+        System.out.println(gato);
+
+        /*int a = 7;
+        int b = 4;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
---
+++ src/br/com/dio/model/Cat.java
@@ -0,0 +1,10 @@
+package br.com.dio.model;
+
+public class Cat {
+    private String nome;
+    private  String cor;
+    private Integer idade;
+
+    public Cat() {
+    }
+}
