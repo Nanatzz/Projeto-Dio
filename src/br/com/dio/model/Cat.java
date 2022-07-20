c136f271122312cc7031df3d8c6da449ece4839d
Nanatzz <natygrott@hotmail.com>
2022-07-20 15:16:04 -0300

    Testando a IDE

--- src/br/com/dio/model/Cat.java
+++ src/br/com/dio/model/Cat.java
@@ -7,4 +7,28 @@ public class Cat {
 
     public Cat() {
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
